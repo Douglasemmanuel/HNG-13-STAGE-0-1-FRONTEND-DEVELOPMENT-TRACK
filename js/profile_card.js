d9be131c39bb7dcc30feb2f9a95147e323c919ba
DouglasEmmanuel <emmanueldouglas2121@gmail.com>
2025-10-16 23:41:28 -0700

    feat: add profile card with user information, social links, hobbies, and dislikes

---
+++ js/profile_card.js
@@ -0,0 +1,21 @@
+
+
+setInterval(()=>{
+    document.querySelector('[data-testid="test-user-time"]').textContent = Date.now();
+}, 1000) ;
+
+
+
+
+function showCurrentTime() {
+  const clock = document.querySelector('[data-testid="test-user-time"]');
+  const now = new Date();
+  const timeString = now.toLocaleTimeString(); // e.g. "10:35:22 AM"
+  clock.textContent = timeString;
+}
+
+// Run once at load
+showCurrentTime();
+
+// Update every second
+setInterval(showCurrentTime, 1000);
